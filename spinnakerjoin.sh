ca13d4d6a61a2add3d639f38c2d67d1d6848b261
suprobhat <61926568+suprobhat@users.noreply.github.com>
2022-07-15 19:55:06 +0530

    Update spinnakerjoin.sh

--- spinnakerjoin.sh
+++ spinnakerjoin.sh
@@ -1,36 +1,6 @@
 #!/bin/bash
 
+gcloud auth activate-service-account --key-file /home/haluser/halserver.json
+gcloud container clusters get-credentials cluster-1 --region us-west1-a --project devopsteamrnd
+kubectl get all
 
-
-checkCluster=$(hal config provider kubernetes account list |grep cluster-1 |awk '{print \$3}')
-
-
-
-if [ -z "\$checkCluster" ];
-
-then
-
-  gcloud auth activate-service-account --key-file /home/haluser/halserver.json
-
-  gcloud container clusters get-credentials cluster-1 --region us-west1-a --project devopsteamrnd;
-  kubectl get all
-
-  CONTEXT=$(kubectl config current-context --kubeconfig ~/.kube/cluster-1);
-
-  TOKEN=$(kubectl get secret --kubeconfig ~/.kube/cluster-1 --context $CONTEXT $(kubectl get serviceaccount spinnaker-sa --kubeconfig ~/.kube/cluster-1 --context \$CONTEXT -n spinnaker -o jsonpath='{.secrets[0].name}') -n spinnaker -o jsonpath='{.data.token}' | base64 --decode);
-
-  kubectl config set-credentials $CONTEXT-token-user --kubeconfig ~/.kube/cluster-1 --token $TOKEN;
-
-  kubectl config set-context $CONTEXT --kubeconfig ~/.kube/cluster-1 --user ${CONTEXT}-token-user;
-
-  hal config provider kubernetes account add cluster-1 --context $CONTEXT --kubeconfig-file ~/.kube/cluster-1;
-
-  hal deploy apply;
-
-else
-
-  echo "Cluster Already Added"
-
-fi
-
-"""
