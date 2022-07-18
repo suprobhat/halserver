ef73d64c0f3f904a084df5dee55d8fcf9c1b5583
suprobhat <61926568+suprobhat@users.noreply.github.com>
2022-07-18 11:43:32 +0530

    Update spinnakerjoin.sh

--- spinnakerjoin.sh
+++ spinnakerjoin.sh
@@ -6,11 +6,17 @@ gcloud container clusters get-credentials cluster-1 --region us-west1-a --projec
 kubectl get all
 gcloud container clusters list
 
-CONTEXT=$(kubectl config current-context --kubeconfig ~/.kube/cluster-1)
+CONTEXT=$(kubectl config current-context)
 echo $CONTEXT
-TOKEN=$(kubectl get secret --kubeconfig ~/.kube/cluster-1 --context $CONTEXT $(kubectl get serviceaccount spinnaker-sa --kubeconfig ~/.kube/cluster-1 --context $CONTEXT -n spinnaker -o jsonpath='{.secrets[0].name}') -n spinnaker -o jsonpath='{.data.token}' | base64 --decode)
-kubectl config set-credentials $CONTEXT-token-user --kubeconfig ~/.kube/cluster-1 --token $TOKEN
-kubectl config set-context $CONTEXT --kubeconfig ~/.kube/cluster-1 --user ${CONTEXT}-token-user
+TOKEN=$(kubectl get secret --context $CONTEXT \
+   $(kubectl get serviceaccount spinnaker-sa \
+       --context $CONTEXT \
+       -n spinnaker \
+       -o jsonpath='{.secrets[0].name}') \
+   -n spinnaker \
+   -o jsonpath='{.data.token}' | base64 --decode)
+kubectl config set-credentials $CONTEXT-token-user --token $TOKEN   
+kubectl config set-context $CONTEXT --user $CONTEXT-token-user
 hal config provider kubernetes account add cluster-1 --context $CONTEXT --kubeconfig-file ~/.kube/cluster-1
 hal deploy apply
 
