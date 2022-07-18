b2a173961bf0e0f6f2c87144da718ff61a3da354
suprobhat <61926568+suprobhat@users.noreply.github.com>
2022-07-18 16:37:55 +0530

    Update spinnakerjoin.sh

--- spinnakerjoin.sh
+++ spinnakerjoin.sh
@@ -4,7 +4,6 @@ gcloud auth activate-service-account --key-file /home/haluser/halserver.json
 sudo apt-get install google-cloud-sdk-gke-gcloud-auth-plugin -y
 gcloud container clusters get-credentials cluster-1 --region us-west1-a --project devopsteamrnd
 kubectl get all
-kubectl create ns spinnaker
 kubectl get ns
 kubectl create sa spinnaker-sa -n spinnaker
 gcloud container clusters list
@@ -16,7 +15,10 @@ echo $TOKEN
 kubectl config set-credentials $CONTEXT-token-user --token $TOKEN   
 kubectl config set-context $CONTEXT --user $CONTEXT-token-user
 hal config provider kubernetes account add spinnaker-sa --context $CONTEXT 
+sudo mkdir ~/.hal/default/profiles/
 echo 'spinnaker.s3.versioning: false' > ~/.hal/default/profiles/front50-local.yml
+kubectl create ns spinnaker
+kubectl get ns
 hal config features edit --artifacts true
 sudo snap install helm --classic -y
 helm repo add stable https://charts.helm.sh/stable 
