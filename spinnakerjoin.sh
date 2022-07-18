75af87d9f7f77371dc149f2ba98ce01181b32b1e
suprobhat <61926568+suprobhat@users.noreply.github.com>
2022-07-18 16:53:25 +0530

    Update spinnakerjoin.sh

--- spinnakerjoin.sh
+++ spinnakerjoin.sh
@@ -5,10 +5,7 @@ sudo apt-get install google-cloud-sdk-gke-gcloud-auth-plugin -y
 gcloud container clusters get-credentials cluster-1 --region us-west1-a --project devopsteamrnd
 kubectl get all
 kubectl get ns
-kubectl create ns spinnaker
-kubectl create sa spinnaker-sa -n spinnaker
 gcloud container clusters list
-kubectl get sa -A
 CONTEXT=$(kubectl config current-context)
 echo $CONTEXT
 TOKEN=$(kubectl get secret --context $CONTEXT $(kubectl get serviceaccount spinnaker-sa --context $CONTEXT -n spinnaker -o jsonpath='{.secrets[0].name}') -n spinnaker -o jsonpath='{.data.token}' | base64 --decode)
@@ -19,6 +16,8 @@ kubectl config get-contexts
 kubectl config use-context gke_devopsteamrnd_us-west1-a_cluster-1
 kubectl config set-cluster gke_devopsteamrnd_us-west1-a_cluster-1
 kubectl get all
+kubectl create ns spinnaker
+kubectl create sa spinnaker-sa -n spinnaker
 hal config provider kubernetes account add spinnaker-sa --context $CONTEXT 
 sudo mkdir ~/.hal/default/profiles/
 echo 'spinnaker.s3.versioning: false' > ~/.hal/default/profiles/front50-local.yml
