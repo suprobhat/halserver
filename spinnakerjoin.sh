12ec94557555bccf998e1a9d8f5da40cf3273638
suprobhat <61926568+suprobhat@users.noreply.github.com>
2022-07-18 14:47:24 +0530

    Update spinnakerjoin.sh

--- spinnakerjoin.sh
+++ spinnakerjoin.sh
@@ -8,11 +8,13 @@ kubectl create ns spinnaker
 kubectl get ns
 gcloud container clusters list
 kubectl get sa -A
-CONTEXT=$(kubectl config current-context --kubeconfig ~/.kube/cluster-1)
-TOKEN=$(kubectl get secret --kubeconfig ~/.kube/cluster-1 --context $CONTEXT $(kubectl get serviceaccount spinnaker-sa --kubeconfig ~/.kube/cluster-1 --context $CONTEXT -n spinnaker -o jsonpath='{.secrets[0].name}') -n spinnaker -o jsonpath='{.data.token}' | base64 --decode)
-kubectl config set-credentials $CONTEXT-token-user --kubeconfig ~/.kube/cluster-1 --token $TOKEN
-kubectl config set-context $CONTEXT --kubeconfig ~/.kube/cluster-1 --user ${CONTEXT}-token-user
-hal config provider kubernetes account add cluster-1 --context $CONTEXT --kubeconfig-file ~/.kube/cluster-1
+TOKEN=$(kubectl get secret --context $CONTEXT \
+   $(kubectl get serviceaccount spinnaker-sa  \
+       --context $CONTEXT \
+       -n spinnaker \
+       -o jsonpath='{.secrets[0].name}') \
+   -n spinnaker \
+   -o jsonpath='{.data.token}' | base64 --decode)
 hal deploy apply
 
 
