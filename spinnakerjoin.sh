6026f91745d970749ffab0cc9532ea15eb6f2205
suprobhat <61926568+suprobhat@users.noreply.github.com>
2022-07-18 17:42:13 +0530

    Update spinnakerjoin.sh

--- spinnakerjoin.sh
+++ spinnakerjoin.sh
@@ -21,7 +21,7 @@ kubectl config set-credentials $CONTEXT-token-user --token $TOKEN
 kubectl config set-context $CONTEXT --user $CONTEXT-token-user
 hal config provider kubernetes enable 
 CONTEXT=$(kubectl config current-context) 
-hal config provider kubernetes account add spinnaker-sa --context $CONTEXT 
+#hal config provider kubernetes account add spinnaker-sa --context $CONTEXT 
 #sudo mkdir -p ~/.hal/default/profiles/
 #chmod -R 777 ~/.hal/default/profiles/
 #echo 'spinnaker.s3.versioning: false' > ~/.hal/default/profiles/front50-local.yml
