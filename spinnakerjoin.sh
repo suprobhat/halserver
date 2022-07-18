41325065e5409f6fdabe81ba2aee864c5767694e
suprobhat <61926568+suprobhat@users.noreply.github.com>
2022-07-18 17:49:09 +0530

    Update spinnakerjoin.sh

--- spinnakerjoin.sh
+++ spinnakerjoin.sh
@@ -18,7 +18,7 @@ echo $CONTEXT
 TOKEN=$(kubectl get secret --context $CONTEXT $(kubectl get serviceaccount spinnaker-sa --context $CONTEXT -n spinnaker -o jsonpath='{.secrets[0].name}') -n spinnaker -o jsonpath='{.data.token}' | base64 --decode)
 echo $TOKEN
 kubectl config set-credentials $CONTEXT-token-user --token $TOKEN   
-kubectl config set-context $CONTEXT --user $CONTEXT-token-user
+#kubectl config set-context $CONTEXT --user $CONTEXT-token-user
 #hal config provider kubernetes enable 
 #CONTEXT=$(kubectl config current-context) 
 #hal config provider kubernetes account add spinnaker-sa --context $CONTEXT 
