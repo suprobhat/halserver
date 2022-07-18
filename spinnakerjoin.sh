8e94272dc80fcda929c2508f3d86a21624fac6dc
suprobhat <61926568+suprobhat@users.noreply.github.com>
2022-07-18 18:38:24 +0530

    Update spinnakerjoin.sh

--- spinnakerjoin.sh
+++ spinnakerjoin.sh
@@ -2,7 +2,7 @@
 
 gcloud auth activate-service-account --key-file /home/haluser/halserver.json
 sudo apt-get install google-cloud-sdk-gke-gcloud-auth-plugin -y
-gcloud container clusters get-credentials cluster-1 --region us-west1-a --project devopsteamrnd
+gcloud container clusters get-credentials cluster-1 --zone us-west1-a --project devopsteamrnd
 kubectl get all
 kubectl get ns
 gcloud container clusters list
