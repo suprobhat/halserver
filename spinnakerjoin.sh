85ced414e2434653e856eceb0552ec40f2f4eb6e
suprobhat <61926568+suprobhat@users.noreply.github.com>
2022-07-18 16:45:19 +0530

    Update spinnakerjoin.sh

--- spinnakerjoin.sh
+++ spinnakerjoin.sh
@@ -14,6 +14,10 @@ TOKEN=$(kubectl get secret --context $CONTEXT $(kubectl get serviceaccount spinn
 echo $TOKEN
 kubectl config set-credentials $CONTEXT-token-user --token $TOKEN   
 kubectl config set-context $CONTEXT --user $CONTEXT-token-user
+kubectl config get-contexts
+kubectl config use-context gke_devopsteamrnd_us-west1-a_cluster-1
+kubectl config set-cluster gke_devopsteamrnd_us-west1-a_cluster-1
+kubectl get all
 hal config provider kubernetes account add spinnaker-sa --context $CONTEXT 
 sudo mkdir ~/.hal/default/profiles/
 echo 'spinnaker.s3.versioning: false' > ~/.hal/default/profiles/front50-local.yml
@@ -23,7 +27,7 @@ hal config features edit --artifacts true
 sudo snap install helm --classic -y
 helm repo add stable https://charts.helm.sh/stable 
 helm repo update 
-helm install minio --namespace spinnaker stable/minio --set accessKey="myaccesskey" --set secretKey="mysecretkey" --values /home/haluser/values.yaml
+helm install minio --namespace spinnaker stable/minio --set accessKey="myaccesskey" --set secretKey="mysecretkey" -f /home/haluser/values.yaml
 hal config storage s3 edit --endpoint http://minio:9000 --access-key-id "myaccesskey" --secret-access-key "mysecretkey"
 mkdir ~/.hal/default/profiles/ 
 hal config deploy edit --type distributed --account-name spinnaker-sa
